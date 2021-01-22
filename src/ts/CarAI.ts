41762f10c4424c487258832b92aa469f9b9e9dfe
Jules Fouchy <jules.fouchy@ntymail.com>
2021-01-22 14:58:46 +0100

    Add a bias to the activation parameters

--- src/ts/CarAI.ts
+++ src/ts/CarAI.ts
@@ -10,15 +10,17 @@ class ActivationParams {
     private c1: number
     private c2: number
     private c3: number
+    private bias: number
 
     constructor() {
-        this.c1 = Math.random() * 2 - 1
-        this.c2 = Math.random() * 2 - 1
-        this.c3 = Math.random() * 2 - 1
+        this.c1   = Math.random() * 2 - 1
+        this.c2   = Math.random() * 2 - 1
+        this.c3   = Math.random() * 2 - 1
+        this.bias = Math.random() * 2 - 1
     }
 
     eval(v1: number, v2: number, v3: number): boolean {
-        return this.c1 * v1 + this.c2 * v2 + this.c3 * v3 > 0
+        return this.c1 * v1 + this.c2 * v2 + this.c3 * v3 + this.bias > 0
     }
 }
 
