060d6ade9105b7f1204675a176836aea7e2dfb43
Jules Fouchy <jules.fouchy@ntymail.com>
2021-02-05 16:48:48 +0100

    Start simple AI model and use it in KartBrain

---
+++ Assets/Scripts/AIModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layer {
+    public Layer()
+    {
+        c1 = Random.value;
+        c2 = Random.value;
+        c3 = Random.value;
+        bias = Random.value;
+    }
+
+    public float eval(float x1, float x2, float x3) {
+        return c1 * x1 + c2 * x2  + c3 * x3 + bias;
+    }
+
+    private float c1;
+    private float c2;
+    private float c3;
+    private float bias;
+} 
+
+public class AIModel
+{
+    public AIModel()
+    {
+        layer1 = new Layer();
+        layer2 = new Layer();
+    }
+
+    public float evalAcceleration(float x1, float x2, float x3) {
+        return layer1.eval(x1, x2, x3);
+    }
+
+    public float evalSteer(float x1, float x2, float x3) {
+        return layer2.eval(x1, x2, x3);
+    }
+
+    Layer layer1;
+    Layer layer2;
+}
--- Assets/Scripts/KartBrain.cs
+++ Assets/Scripts/KartBrain.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(KartController))]
+[RequireComponent(typeof(KartController)), RequireComponent(typeof(RayCastSensors))]
 public class KartBrain : MonoBehaviour {
 
     [HideInInspector]
     public KartController kartController;
+    public RayCastSensors rayCastSensors;
 
     private void Awake() {
         kartController = GetComponent<KartController>();
+        rayCastSensors = GetComponent<RayCastSensors>();
     }
 
     private void Update() {
-        kartController.ApplyAcceleration(Random.Range(0.0f, 1.0f));
-        kartController.Steer(Random.Range(-1.0f, 1.0f));
+        var hits = rayCastSensors.GetHitInformations();
+        kartController.ApplyAcceleration(aiModel.evalAcceleration(hits[0].distance, hits[1].distance, hits[2].distance));
+        kartController.Steer(aiModel.evalSteer(hits[0].distance, hits[1].distance, hits[2].distance));
     }
+
+    private AIModel aiModel;
 }
