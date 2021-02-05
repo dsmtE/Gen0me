af139d734b2d03c938bbfe1dd5926142826e17b5
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2021-02-05 16:49:02 +0100

    clean rayCastSensors script

--- Assets/Scripts/RayCastSensors.cs
+++ Assets/Scripts/RayCastSensors.cs
@@ -7,55 +7,27 @@ public class RayCastSensors : MonoBehaviour {
     [Range(0, 50)]
     [Tooltip("Number of rays to the left and right of center.")]
     [SerializeField] private int raysPerDirection = 3;
-    public int RaysPerDirection {
-        get => raysPerDirection;
-        set { raysPerDirection = value; UpdateHitInformations(); }
-    }
 
     [Range(0, 180)]
     [Tooltip("Cone size for rays. Using 90 degrees will cast rays to the left and right. " +
     "Greater than 90 degrees will go backwards.")]
     [SerializeField] private float maxRayDegrees = 70;
-    public float MaxRayDegrees {
-        get => maxRayDegrees;
-        set { maxRayDegrees = value; UpdateHitInformations(); }
-    }
 
     [Range(1, 1000)]
     [Tooltip("Length of the rays to cast.")]
-    [SerializeField] float rayLength = 20f;
-    public float RayLength {
-        get => rayLength;
-        set { rayLength = value; UpdateHitInformations(); }
-    }
+    [SerializeField] private float rayLength = 20f;
 
     [Tooltip("Starting rays orientation")]
-    [SerializeField] Vector3 rayOrientation = Vector3.zero;
-    public Vector3 RayOrientation {
-        get => rayOrientation;
-        set { rayOrientation = value; UpdateHitInformations(); }
-    }
+    [SerializeField] private Vector3 rayOrientation = Vector3.zero;
 
     [Tooltip("Starting offset position of the rays to cast.")]
-    [SerializeField] Vector3 startOffset = Vector3.zero;
-    public Vector3 StartOffset {
-        get => startOffset;
-        set { startOffset = value; UpdateHitInformations(); }
-    }
+    [SerializeField] private Vector3 startOffset = Vector3.zero;
 
     [Tooltip("Ending offset position of the rays to cast.")]
-    [SerializeField] Vector3 endOffset = Vector3.zero;
-    public Vector3 EndOffset {
-        get => endOffset;
-        set { endOffset = value; UpdateHitInformations(); }
-    }
+    [SerializeField] private Vector3 endOffset = Vector3.zero;
 
     [Tooltip("Controls which layers the rays can hit.")]
-    [SerializeField] LayerMask rayLayerMask = Physics.DefaultRaycastLayers;
-    public LayerMask RayLayerMask {
-        get => rayLayerMask;
-        set { rayLayerMask = value; UpdateHitInformations(); }
-    }
+    [SerializeField] private LayerMask rayLayerMask = Physics.DefaultRaycastLayers;
 
     [Header("Debug Gizmos", order = 999)]
     [SerializeField] internal Color rayHitColor = Color.red;
@@ -66,11 +38,8 @@ public class RayCastSensors : MonoBehaviour {
         public bool hasHit;
         public GameObject hitObject;
         public float distance;
-        public Vector3 direction; // non normalized
+        public Vector3 direction;
     }
-
-    HitInformation[] hitInformations = null;
-    private bool updatedInformations = false;
 
     private float[] GetRayAngles(int raysPerDirection, float maxRayDegrees) {
         // Example:
@@ -86,40 +55,30 @@ public class RayCastSensors : MonoBehaviour {
     }
 
     public HitInformation[] GetHitInformations() {
-        if (updatedInformations) {
-            return hitInformations;
-        } else {
-            return UpdateHitInformations();
-        }
-    }
-    private HitInformation[] UpdateHitInformations() {
-        // change size if needed
-        if (hitInformations == null || hitInformations.Length != 2 * RaysPerDirection + 1) {
-            hitInformations = new HitInformation[2 * RaysPerDirection + 1];
-        }
+        HitInformation[] hitInformations = new HitInformation[2 * raysPerDirection + 1];
 
-        Vector3 startPos = transform.TransformPoint(StartOffset);
-        float[] raysAngles = GetRayAngles(RaysPerDirection, MaxRayDegrees);
+        Vector3 startPos = transform.TransformPoint(startOffset);
+        float[] raysAngles = GetRayAngles(raysPerDirection, maxRayDegrees);
 
         for (int i = 0; i < hitInformations.Length; ++i) {
 
-            Vector3 endPos = startPos + transform.rotation * (Quaternion.Euler(RayOrientation) * Quaternion.AngleAxis(raysAngles[i], Vector3.up) * Vector3.forward) * RayLength + EndOffset;
+            Vector3 endPos = startPos + transform.rotation * (Quaternion.Euler(rayOrientation) * Quaternion.AngleAxis(raysAngles[i], Vector3.up) * Vector3.forward) * rayLength + endOffset;
 
             Vector3 rayDirection = Vector3.Normalize(endPos - startPos);
 
             RaycastHit rayHit;
-            bool hasHit = Physics.Raycast(startPos, rayDirection, out rayHit, RayLength, RayLayerMask);
+            bool hasHit = Physics.Raycast(startPos, rayDirection, out rayHit, rayLength, rayLayerMask);
             GameObject hitObject = hasHit ? rayHit.collider.gameObject : null;
             float distance = hasHit ? rayHit.distance : 10000f;
 
             hitInformations[i] = new HitInformation { hasHit = hasHit, hitObject = hitObject, distance = distance, direction = rayDirection };
         }
-        updatedInformations = true;
+
         return hitInformations;
     }
 
     void OnDrawGizmosSelected() {
-        foreach (HitInformation hi in UpdateHitInformations()) {
+        foreach (HitInformation hi in GetHitInformations()) {
             DrawRaycastGizmos(hi);
         }
     }
@@ -130,11 +89,11 @@ public class RayCastSensors : MonoBehaviour {
         Color color = hi.hasHit ? rayHitColor : rayMissColor;
         color.a *= alpha;
         Gizmos.color = color;
-        Gizmos.DrawRay(transform.TransformPoint(StartOffset), hi.direction * (hi.hasHit ? hi.distance : rayLength));
+        Gizmos.DrawRay(transform.TransformPoint(startOffset), hi.direction * (hi.hasHit ? hi.distance : rayLength));
 
         // Draw the hit point as a sphere. If using rays to cast (0 radius), use a small sphere.
         if (hi.hasHit) {
-            Gizmos.DrawWireSphere(transform.TransformPoint(StartOffset) + hi.direction * hi.distance, displayHitSphereRadius);
+            Gizmos.DrawWireSphere(transform.TransformPoint(startOffset) + hi.direction * hi.distance, displayHitSphereRadius);
         }
     }
 }
