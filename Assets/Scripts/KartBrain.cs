8dde2ff19302e1f2214dee521d6733305d2ea0b9
Enguerrand DE SMET <desmet.enguerrand@gmail.com>
2021-02-05 16:28:18 +0100

    Add random kart brain and Training manager (spawning)

---
+++ Assets/Scripts/KartBrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(KartController))]
+public class KartBrain : MonoBehaviour {
+
+    [HideInInspector]
+    public KartController kartController;
+
+    private void Awake() {
+        kartController = GetComponent<KartController>();
+    }
+
+    private void Update() {
+        kartController.ApplyAcceleration(Random.Range(0.0f, 1.0f));
+        kartController.Steer(Random.Range(-1.0f, 1.0f));
+    }
+}
---
+++ Assets/Scripts/TrainingManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingManager : MonoBehaviour {
+
+    public int entitiesNumber = 10;
+
+    private KartBrain[] brainsList;
+
+    [SerializeField]
+    private GameObject kartPrefab;
+
+    private GameObject[] spawnPoints;
+
+
+    private void Awake() {
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+
+        brainsList = new KartBrain[entitiesNumber];
+
+        for(int i = 0; i < entitiesNumber; ++i) {
+
+            Transform sp = SelectRandomSpawnpoint();
+
+            GameObject go = Instantiate(kartPrefab, sp.position, sp.rotation);
+            go.transform.parent = transform;
+
+            go.transform.localScale = Vector3.one;
+            brainsList[i] = go.GetComponent<KartBrain>();
+        }
+    }
+    public void SpawnKart() {
+        foreach (KartBrain kb in brainsList) {
+            kb.kartController.Respawn(SelectRandomSpawnpoint());
+        }  
+    }
+
+    private Transform SelectRandomSpawnpoint() {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)].transform;
+    }
+
+    private void Start() {
+        SpawnKart();
+    }
+}
